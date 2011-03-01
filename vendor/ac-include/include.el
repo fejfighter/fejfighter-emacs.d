5b8c981655862a6fe18f6ff6c193cfea179944c3
Jeff Walsh <fejfighter@gmail.com>
2011-03-01 13:10:46 +1100

    fix filename spelling get usr/include files to be listed

---
+++ vendor/ac-include/include.el
@@ -0,0 +1,16 @@
+
+
+(defvar global-candidates-cache nil)
+
+(defun global-candidates-init ()
+  ( setq global-candidates-cache (concat "<" (directory-files "/usr/include/") ">"))
+
+
+
+(defvar ac-source-include
+'((init . global-candidates-init) 
+  (candidates . global-candidates-cache)
+  (prefix . "#include \\(.*\\)")))
+
+
+(setq ac-sources '(ac-source-include))
