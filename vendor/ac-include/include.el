a6933a2bf744e326005a9c912de6a4724b51117c
Jeff Walsh <fejfighter@gmail.com>
2011-03-04 13:48:21 +1100

    flesh out more code for ac-include

--- vendor/ac-include/include.el
+++ vendor/ac-include/include.el
@@ -1,16 +1,31 @@
 
+(provide 'auto-complete-include)
 
-(defvar global-candidates-cache nil)
+(defvar ac-include-candidates-cache nil)
 
-(defun global-candidates-init ()
-  ( setq global-candidates-cache (concat "<" (directory-files "/usr/include/") ">"))
+(defun ac-inculde-candidates-init ()
+  ( setq global-candidates-cache (concat "<" (directory-files "/usr/include/") ">")))
+
+(defun ac-include-document ()
+  )
 
 
+;(defun ac-include-prefix ()
+ ; (prefix . "#include \\(.*\\)")))
 
-(defvar ac-source-include
-'((init . global-candidates-init) 
-  (candidates . global-candidates-cache)
-  (prefix . "#include \\(.*\\)")))
+;(defvar ac-source-include
+;'( 
+;  (candidates . global-candidates-cache)
+;  
 
+(ac-define-source include
+  '((init . global-candidates-init)
+    (candidates . global-candidates-cache)
+    (prefix . "#include \\(.*\\)")
+    (requires . 0)
+    (document . ac-include-document)
+;    ( action . ac-include-action)
+    (cache)
+    (symbol . "i")))
 
 (setq ac-sources '(ac-source-include))
