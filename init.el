a3e08c59111f0d3c32300c1b32cd84bc0d68fb2a
Jeff Walsh <fejfighter@gmail.com>
2012-08-20 14:25:48 +1000

    update submodules, add inits for cweb mode

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
 (load-file "~/.emacs.d/vendor/cedet/cedet-devel-load.el")
+(add-to-list 'load-path "~/.emacs.d/vendor/mmm")
+(load "~/.emacs.d/vendor/mmm/mmm-auto.el")
+
 ;
 
 (add-to-list 'load-path "~/.emacs.d")
@@ -41,6 +44,11 @@
 (load "fejfighter/lua-mode")
 
 (vendor 'magit)
+
+;(vendor 'mmm)
+
+
+(vendor 'slime)
 
 (vendor 'todo)
 
