b46dfda09605d2f1d3a0c90f2f0f128af716194f
Jeff Walsh <fejfighter@gmail.com>
2013-03-05 08:19:59 +1100

    cedet/ecb updates

--- fejfighter/cedet-conf.el
+++ fejfighter/cedet-conf.el
@@ -22,21 +22,29 @@
 (require 'semantic/decorate/include)
 (require 'semantic/lex-spp)
 (require 'semantic/java)
+
 ;(require 'semantic/db-javap)
 ;(require 'eassist)
 
-;(semanticdb-enable-gnu-global-databases 'c-mode t)
-;(semanticdb-enable-gnu-global-databases 'c++-mode t)
+(setq semantic-load-turn-useful-things-on t)
+
+(semantic-add-system-include "/usr/local/include/" 'c-mode)
+(semantic-add-system-include "/usr/local/include/" 'c++-mode)
+
+(setq cedet-global-command "global") ; Change to path of global as needed
+(when (cedet-gnu-global-version-check t)
+  (semanticdb-enable-gnu-global-databases 'c-mode t)
+  (semanticdb-enable-gnu-global-databases 'c++-mode t))
 
 ;(when (cedet-ectag-version-check t)
 ;  (semantic-load-enable-primary-ectags-support))
 
 ;; SRecode
-;(global-srecode-minor-mode 1)
+(global-srecode-minor-mode 1)
 
 ;; EDE
-;(global-ede-mode 1)
-;(ede-enable-generic-projects)
+(global-ede-mode 1)
+(ede-enable-generic-projects)
 
 ;; setup compile package
 (require 'compile)
@@ -44,8 +52,36 @@
 (setq compilation-scroll-output t)
 (setq mode-compile-always-save-buffer-p t)
 
-;; android
-;(cedet-android-sdk-root /opt/homebrew/Cellar/android-sdk/r20.0.3)
+;; OPENCV config
+(add-to-list 'semantic-lex-c-preprocessor-symbol-file 
+         '"/usr/local/include/opencv2/core/types_c.h")
+(add-to-list 'semantic-lex-c-preprocessor-symbol-file 
+         '"/usr/local/include/opencv2/imgproc/types_c.h")
 
-;;; emacs-rc-cedet.el ends here
+(add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CV_PROP_RW" . ""))
+(add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CV_EXPORTS" . ""))
+(add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CV_EXPORTS_W_SIMPLE" . ""))
+(add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CV_EXPORTS_W" . ""))
+(add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CV_EXPORTS_W_MAP" . ""))
+(add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CV_INLINE" . ""))
 
+;; ALEX OTT hooks, 
+(defun my-cedet-hook ()
+  (local-set-key [(control return)] 'semantic-ia-complete-symbol)
+  (local-set-key "\C-c?" 'semantic-ia-complete-symbol-menu)
+  (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
+  (local-set-key "\C-c=" 'semantic-decoration-include-visit)
+  (local-set-key "\C-cj" 'semantic-ia-fast-jump)
+  (local-set-key "\C-cq" 'semantic-ia-show-doc)
+  (local-set-key "\C-cs" 'semantic-ia-show-summary)
+  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
+;  (local-set-key "\C-c+" 'semantic-tag-folding-show-block)
+;  (local-set-key "\C-c-" 'semantic-tag-folding-fold-block)
+;  (local-set-key "\C-c\C-c+" 'semantic-tag-folding-show-all)
+;  (local-set-key "\C-c\C-c-" 'semantic-tag-folding-fold-all)
+  )
+(add-hook 'c-mode-common-hook 'my-cedet-hook)
+(add-hook 'emacs-lisp-mode-hook 'my-cedet-hook)
+(add-hook 'lisp-interaction-mode-hook 'my-cedet-hook)
+
+;(global-semantic-tag-folding-mode 1)
