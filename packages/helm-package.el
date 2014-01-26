c174d313364cff09a7f7666df05f6a8da7f7171b
Jeff Walsh <fejfighter@gmail.com>
2014-01-27 09:34:55 +1100

    add some initial keybindings for Helm-mode

--- packages/helm-package.el
+++ packages/helm-package.el
@@ -1,7 +1,11 @@
-
 (setq el-get-sources
  (append
-  '((:name helm)
+  '((:name helm
+	   :after (progn
+		    (global-set-key (kbd "S-h k") 'helm-show-kill-ring)
+		    (global-set-key (kbd "C-c h f") 'helm-find-files)
+		    (global-set-key (kbd "C-c h") 'helm-mini)
+		    ))
     (:name helm-c-yasnippet)
     (:name helm-helm-commands)
     )
@@ -9,3 +13,4 @@
 
 
 (provide 'helm-package)
+  
