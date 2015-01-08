e9de71f382de5ff3f076d40a13b62b2bda98fcc6
fejfighter <fejfighter@gmail.com>
2015-01-08 20:33:07 +1100

    Move to cedet git, added as a git-submodule

--- init.el
+++ init.el
@@ -2,6 +2,13 @@
 (server-start)
 ;(setq debug-on-error t)
 
+(add-to-list 'load-path (concat user-emacs-directory "fejfighter"))
+(add-to-list 'load-path (concat user-emacs-directory "vendor"))
+
+;; cedet-git needs to be loaded before anything else
+(load-file (concat user-emacs-directory "vendor/cedet-git/cedet-devel-load.el"))
+
+
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
@@ -16,8 +23,6 @@
 ; install and load the packages
 ;(require 'package-list)
 
-(add-to-list 'load-path (concat user-emacs-directory "fejfighter"))
-;(add-to-list 'load-path (concat user-emacs-directory "vendor"))
 
 ; add the local changes
 (require 'fejfighter-init)
