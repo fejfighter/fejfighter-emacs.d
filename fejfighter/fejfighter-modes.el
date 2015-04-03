def2ee0aed41eae543899adbd31e9caa56c3d246
Jeff Walsh <jaw988@uowmail.edu.au>
2015-04-03 22:13:30 +1100

    commit latest macbook changes

--- fejfighter/fejfighter-modes.el
+++ fejfighter/fejfighter-modes.el
@@ -1,20 +1,27 @@
 ;; Python
-					
-					; scons
+; scons
 (setq auto-mode-alist
       (cons '("SConstruct" . python-mode) auto-mode-alist))
 (setq auto-mode-alist
       (cons '("SConscript" . python-mode) auto-mode-alist))
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Utilities
 ;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Cider
+(require 'cider)
+
 ;; helm
 (require 'fejfighter-helm)
 
+(require 'flycheck)
+(global-flycheck-mode t)
 ;; auto completion
 (require 'company)
 (global-company-mode t)
+
+(global-set-key (kbd "M-/") 'company-complete-common)
 
 (require 'fejfighter-cedet)
 
