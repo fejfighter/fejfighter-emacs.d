586cf7e6aac74d43a85657fa031142f476c93ea2
Jeff Walsh <jaw988@uowmail.edu.au>
2017-07-06 22:07:53 +1000

    Move to lsp mode (sort of)

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
@@ -15,7 +15,11 @@
 (require 'diminish)
 (require 'bind-key)
 
-(load-file (concat user-emacs-directory "fejfighter-cedet.el"))
+(add-to-list 'load-path (concat user-emacs-directory "vendor/"))
+(add-to-list 'load-path (concat user-emacs-directory "vendor/cedet/"))
+
+;(load-file (concat user-emacs-directory "fejfighter-cedet.el"))
+(load-file (concat user-emacs-directory "fejfighter-lsp.el"))
 (load-file (concat user-emacs-directory "fejfighter-packages.el"))
 (load-file (concat user-emacs-directory "fejfighter-init.el"))
 
