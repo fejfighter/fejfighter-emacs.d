8f8e91accac4b4b5a1e07a60dc6de6dc0ac7b745
Jeff Walsh <fejfighter@gmail.com>
2021-05-04 22:40:16 +1000

    Return to helm

--- fejfighter-packages.el
+++ fejfighter-packages.el
@@ -35,51 +35,73 @@
 (straight-use-package 'cmake-mode)
 (straight-use-package '(tramp :type git :url git://git.savannah.gnu.org/tramp.git ))
 
-(use-package swiper
-  :bind (("M-s" . swiper)) )
+(use-package helm
+  :bind (("M-x" . helm-M-x)
+	 ("C-x C-f" . helm-find-files)
+	 ("M-i" . helm-imenu)
+	 ("<f1> a" . helm-apropos)))
 
-(use-package counsel
-  :diminish (ivy-mode . "")
-  :init
-  (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-  :bind
-  (
-  ("M-x" . counsel-M-x)
-  ("C-x C-f" . counsel-find-file)
-  ("<f1> f" . counsel-describe-function)
-  ("<f1> v" . counsel-describe-variable)
-  ("<f1> l" . counsel-load-library)
-  ("<f2> i" . counsel-info-lookup-symbol)
-  ("<f2> u" . counsel-unicode-char)
-  ("C-c g" . counsel-git)
-  ("C-c j" . counsel-git-grep)
-  ("C-c k" . counsel-ag)
-  ("C-x l" . counsel-locate)
-  ("M-i" . counsel-imenu)
-  ("M-/" . counsel-company))
+(use-package helm-swoop
+  :bind (("M-s" . helm-swoop)))
+
+(use-package helm-posframe
+  :init (helm-posframe-enable))
+
+(use-package helm-company
+  :bind (("M-/" . helm-company)))
+
+(use-package helm-lsp
   :config
-  (setq ivy-use-virtual-buffers t)       ; extend searching to bookmarks and
-  (setq ivy-height 20)                   ; set height of the ivy window
-  (setq ivy-count-format "(%d/%d) ")     ; count format, from the ivy help page
-  (setq ivy-display-style 'fancy)
-  (setq ivy-format-function 'ivy-format-function-line) ; Make highlight extend all the way to the right)
-  )
+  (helm-projectile-on)
 
-(use-package ivy
-  :init
-  (progn
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t))
-  :bind  (("C-c C-r" . ivy-resume)
-	  ("<f6>" . ivy-resume)))
+(use-package helm-projectile)
 
-(use-package ivy-posframe
-  :init (ivy-posframe-mode)
-  )
+;; (use-package swiper
+;;   :bind (("M-s" . swiper)) )
+
+;; (use-package counsel
+;;   :diminish (ivy-mode . "")
+;;   :init
+;;   (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+;;   :bind
+;;   (
+;;   ;("M-x" . counsel-M-x)
+;;   ;("C-x C-f" . counsel-find-file)
+;;   ;("<f1> f" . counsel-describe-function)
+;;   ;("<f1> v" . counsel-describe-variable)
+;;   ("<f1> l" . counsel-load-library)
+;;   ("<f2> i" . counsel-info-lookup-symbol)
+;;   ("<f2> u" . counsel-unicode-char)
+;;   ("C-c g" . counsel-git)
+;;   ("C-c j" . counsel-git-grep)
+;;   ("C-c k" . counsel-ag)
+;;   ("C-x l" . counsel-locate)
+;;   ;("M-i" . counsel-imenu)
+;;   ;("M-/" . counsel-company))
+;;   ;; :config
+;;   ;; (setq ivy-use-virtual-buffers t)       ; extend searching to bookmarks and
+;;   ;; (setq ivy-height 20)                   ; set height of the ivy window
+;;   ;; (setq ivy-count-format "(%d/%d) ")     ; count format, from the ivy help page
+;;   ;; (setq ivy-display-style 'fancy)
+;;   ;; (setq ivy-format-function 'ivy-format-function-line) ; Make highlight extend all the way to the right)
+;;   )
+
+;; (use-package ivy
+;;   :init
+;;   (progn
+;;     (ivy-mode 1)
+;;     (setq ivy-use-virtual-buffers t))
+;;   :bind  (("C-c C-r" . ivy-resume)
+;; 	  ("<f6>" . ivy-resume)))
+
+;; (use-package ivy-posframe
+;;   :init (ivy-posframe-mode)
+;;   )
 
 (use-package imenu-anywhere)
 
 (use-package multiple-cursors
+  :defer
   :init
   (multiple-cursors-mode t))
 
