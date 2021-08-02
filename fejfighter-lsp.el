f3bfa698c748b7fa635f33c51d717ecab5edf3a6
Jeff Walsh <fejfighter@gmail.com>
2021-08-02 23:12:35 +1000

    I don't use go....
    I haven't played with it for a while either

--- fejfighter-lsp.el
+++ fejfighter-lsp.el
@@ -20,13 +20,6 @@
   :config
   (setq lsp-clients-clangd-args '("-j=3" "--background-index" "--compile-commands-dir=.")))
 
-;; Set up before-save hooks to format buffer and add/delete imports.
-;; Make sure you don't have other gofmt/goimports hooks enabled.
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
-(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
-
 (use-package lsp-ui
   :commands lsp-ui-mode
   :hook lsp
