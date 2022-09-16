2e2cf1a835fe5b1b205a14280dd0d1b88a87d5e3
Jeff Walsh <fejfighter@gmail.com>
2022-09-16 15:18:29 +1000

    Use tempel for templating functions
    * fejfighter-packages.el (tempel): template engine
    (eglot-tempel): adaptor for eglot

--- fejfighter-packages.el
+++ fejfighter-packages.el
@@ -110,6 +110,47 @@
   :custom
   (eglot-events-buffer-size 0))
 
+(use-package eglot-tempel
+  :after eglot
+  :straight (eglot-tempel :type git
+			   :host github
+			   :repo "fejfighter/eglot-tempel")
+  :defer 2)
+
+;; Configure Tempel
+(use-package tempel
+  ;; Require trigger prefix before template name when completing.
+  ;; :custom
+  ;; (tempel-trigger-prefix "<")
+
+  :bind (("M-+" . tempel-complete) ;; Alternative tempel-expand
+         ("M-*" . tempel-insert))
+
+  :init
+
+  ;; Setup completion at point
+  (defun tempel-setup-capf ()
+    ;; Add the Tempel Capf to `completion-at-point-functions'.
+    ;; `tempel-expand' only triggers on exact matches. Alternatively use
+    ;; `tempel-complete' if you want to see all matches, but then you
+    ;; should also configure `tempel-trigger-prefix', such that Tempel
+    ;; does not trigger too often when you don't expect it. NOTE: We add
+    ;; `tempel-expand' *before* the main programming mode Capf, such
+    ;; that it will be tried first.
+    (setq-local completion-at-point-functions
+                (cons #'tempel-expand
+                      completion-at-point-functions)))
+
+  (add-hook 'prog-mode-hook 'tempel-setup-capf)
+  (add-hook 'text-mode-hook 'tempel-setup-capf)
+
+  ;; Optionally make the Tempel templates available to Abbrev,
+  ;; either locally or globally. `expand-abbrev' is bound to C-x '.
+  ;; (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+  ;; (global-tempel-abbrev-mode)
+)
+
+
 (use-package toolbox-tramp
   :straight (toolbox-tramp :type git
 			   :host github
