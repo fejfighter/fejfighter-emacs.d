705140e4c1b4b3fd756ca121a6ff0aa024fd6189
Jeff Walsh <fejfighter@gmail.com>
2023-06-13 11:12:11 +1000

    Make changes to improve startup times
    * early-init.el (gc-cons-threshold):
    (package-user-dir):
    (default-frame-alist):

--- early-init.el
+++ early-init.el
@@ -4,20 +4,80 @@
 ;; before package and UI initialization happens.
 
 ;; Defer garbage collection further back in the startup process
-(setq gc-cons-threshold most-positive-fixnum)
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
 
 ;; In Emacs 27+, package initialization occurs before `user-init-file' is
 ;; loaded, but after `early-init-file'. We need to set
 (require 'xdg)
-(setq package-user-dir (concat (xdg-state-home) "/emacs/elpa"))
-(setq package-gnupghome-dir (concat (xdg-state-home) "/elpa"))
-(setq package-quickstart-file (concat (xdg-state-home) "/package-quickstart.el"))
+(require 'package)
+(setq package-user-dir (concat (xdg-state-home) "/emacs/elpa")
+      package-gnupghome-dir (concat (xdg-state-home) "/elpa")
+      package-quickstart-file (concat (xdg-state-home) "/package-quickstart.el")
+      package-enable-at-startup nil
+      package-quickstart t)
 
 ;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
-(setq tool-bar-mode nil
-      menu-bar-mode nil)
-(when (fboundp 'set-scroll-bar-mode)
-  (set-scroll-bar-mode nil))
+(setq default-frame-alist
+      '((menu-bar-lines . 0)
+        (tool-bar-lines . 0)
+        (horizontal-scroll-bars)
+        (vertical-scroll-bars)))
+
+
+(setq load-prefer-newer noninteractive)
+
+(unless (or (daemonp) noninteractive)
+  (let ((old-file-name-handler-alist file-name-handler-alist))
+    ;; `file-name-handler-alist' is consulted on each `require', `load' and
+    ;; various path/io functions. You get a minor speed up by unsetting this.
+    ;; Some warning, however: this could cause problems on builds of Emacs where
+    ;; its site lisp files aren't byte-compiled and we're forced to load the
+    ;; *.el.gz files (e.g. on Alpine).
+    (setq-default file-name-handler-alist nil)
+    ;; ...but restore `file-name-handler-alist' later, because it is needed for
+    ;; handling encrypted or compressed files, among other things.
+    (defun doom-reset-file-handler-alist-h ()
+      (setq file-name-handler-alist
+            ;; Merge instead of overwrite because there may have bene changes to
+            ;; `file-name-handler-alist' since startup we want to preserve.
+            (delete-dups (append file-name-handler-alist
+                                 old-file-name-handler-alist))))
+    (add-hook 'emacs-startup-hook #'doom-reset-file-handler-alist-h 101))
+
+  ;; Premature redisplays can substantially affect startup times and produce
+  ;; ugly flashes of unstyled Emacs.
+  (setq-default inhibit-redisplay t
+                inhibit-message t)
+  (add-hook 'window-setup-hook
+            (lambda ()
+              (setq-default inhibit-redisplay nil
+                            inhibit-message nil)
+              (redisplay)))
+
+  ;; Site files tend to use `load-file', which emits "Loading X..." messages in
+  ;; the echo area, which in turn triggers a redisplay. Redisplays can have a
+  ;; substantial effect on startup times and in this case happens so early that
+  ;; Emacs may flash white while starting up.
+  (define-advice load-file (:override (file) silence)
+    (load file nil 'nomessage))
+
+  ;; Undo our `load-file' advice above, to limit the scope of any edge cases it
+  ;; may introduce down the road.
+  (define-advice startup--load-user-init-file (:before (&rest _) init-doom)
+    (advice-remove #'load-file #'load-file@silence)))
+
+
+;;
+;;; Bootstrap
+
+;; Contrary to what many Emacs users have in their configs, you don't need
+;; more than this to make UTF-8 the default coding system:
+(set-language-environment "UTF-8")
+
+;; set-language-enviornment sets default-input-method, which is unwanted
+(setq default-input-method nil)
+
 
 ;; Resizing the Emacs frame can be a terribly expensive part of changing the
 ;; font. By inhibiting this, we easily halve startup times with fonts that are
@@ -29,4 +89,4 @@
 ;; cursor color is concerned).
 (advice-add #'x-apply-session-resources :override #'ignore)
 
-
+(provide 'early-init)
--- init.el
+++ init.el
@@ -24,3 +24,15 @@
 (fej-req "init")
 (fej-req "work" 'noerror)
 
+(defun my-cleanup-gc ()
+  "Clean up gc."
+  (setq gc-cons-threshold  67108864) ; 64M
+  (setq gc-cons-percentage 0.1) ; original value
+  (garbage-collect))
+
+(run-with-idle-timer 4 nil #'my-cleanup-gc)
+
+(message "*** Emacs loaded in %s with %d garbage collections."
+           (format "%.2f seconds"
+                   (float-time (time-subtract after-init-time before-init-time)))
+           gcs-done)
